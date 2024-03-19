72f05631bd3cc1160f1fe907e585b41a85e97b7f
shivam6862 <shivam6862mau@gmail.com>
2024-03-19 12:48:48 +0530

    diagnosis prediction and form page

--- frontend/src/app/form/[name]/page.tsx
+++ frontend/src/app/form/[name]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import classes from "@/styles/formpage.module.css";
+import Form from "@/components/form/Form";
+import dummyData from "./data.json";
 
 type Props = {
   params: {
@@ -9,9 +11,28 @@ type Props = {
 };
 
 const Page: React.FC<Props> = ({ params: { name } }) => {
+  const foundItem: any = dummyData.find((item) => {
+    return item.hasOwnProperty(name);
+  });
+
   return (
-    <div className={classes["container"]}>
-      <div className={classes["box"]}>Form page for the app {name}...</div>
+    <div className={classes.container}>
+      <div className={classes.box}>
+        <div className={classes.top_description}>
+          <div className={classes.description_box}>
+            <h1>Tell us about your diagnosis ,{name}</h1>
+            <p>
+              Please fill out the form below to tell us about your diagnosis.
+            </p>
+          </div>
+        </div>
+        <div className={classes.middle_background}></div>
+        <div className={classes.bottom_form}>
+          <div className={classes.box_form}>
+            <Form formData={foundItem ? foundItem[name] : {}} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
